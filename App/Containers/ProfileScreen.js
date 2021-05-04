ffb36778ba4826d1b09adf710faeafcfbf85f681
Drew Woodmansee <eesnamdoow@gmail.com>
2021-05-04 16:12:29 -0600

    initial commit

--- App/Containers/ProfileScreen.js
+++ App/Containers/ProfileScreen.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react'
-import {ScrollView, Text} from 'react-native'
-import {connect} from 'react-redux'
+import React, {Component} from 'react';
+import {ScrollView, Text} from 'react-native';
+import {connect} from 'react-redux';
+import FullButton from '../Components/FullButton';
+import NavBar from '../Components/NavBar';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
 // Styles
-import styles from './Styles/ProfileScreenStyle'
+import styles from './Styles/ProfileScreenStyle';
 
 class ProfileScreen extends Component {
   // constructor (props) {
@@ -13,21 +15,34 @@ class ProfileScreen extends Component {
   //   this.state = {}
   // }
 
-  render () {
+  render() {
+    // const styles = StyleSheet.create
     return (
       <ScrollView style={styles.container}>
         <Text>ProfileScreen Container</Text>
+        <FullButton
+          text="Learn More"
+          onPress={() => window.alert('you did it!')}
+        />
+        <FullButton
+          text="All Activites"
+          onPress={() => window.alert('you did it!')}
+        />
+        <NavBar />
       </ScrollView>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
-  return {}
-}
+  return {};
+};
 
 const mapDispatchToProps = dispatch => {
-  return {}
-}
+  return {};
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileScreen)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProfileScreen);
